018e04fdeccee015d752e5a4afa063e4ba65f25c
oliveiraluiz191 <oliveiraluiz191@gmail.com>
2025-02-27 18:24:01 -0300

    atualização utilizando o try

--- java-terminal/src/AboutMe.java
+++ java-terminal/src/AboutMe.java
@@ -9,23 +9,23 @@ public class AboutMe {
     int idade = Integer.valueOf(args[2]);
     double altura = Double.valueOf(args[3]);*/
 
-    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+    try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+      System.out.println("Digite seu nome");
+      String nome = scanner.next();
 
-    System.out.println("Digite seu nome");
-    String nome = scanner.next();
+      System.out.println("Digite seu sobrenome");
+      String sobrenome = scanner.next();
 
-    System.out.println("Digite seu sobrenome");
-    String sobrenome = scanner.next();
+      System.out.println("Digite sua idade");
+      int idade = scanner.nextInt();
 
-    System.out.println("Digite sua idade");
-    int idade = scanner.nextInt();
+      System.out.println("Digite sua altura");
+      double altura = scanner.nextDouble();
 
-    System.out.println("Digite sua altura");
-    double altura = scanner.nextDouble();
-
-    System.out.println("Olá, me chamo " + nome + " " + sobrenome );
-    System.out.println("Tenho " + idade + " anos ");
-    System.out.println("Minha altura é " + altura + "cm");    
+      System.out.println("Olá, me chamo " + nome + " " + sobrenome );
+      System.out.println("Tenho " + idade + " anos ");
+      System.out.println("Minha altura é " + altura + "cm");
+    }    
 
   }
 }
